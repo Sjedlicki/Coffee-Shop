99655f3a64a6121a110029a8409fb55a9d92e7c8
stevojed <jedi1014@comcast.net>
2019-02-13 18:40:05 -0500

    Adding validation and such

--- Coffee Shop/Controllers/HomeController.cs
+++ Coffee Shop/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Web;
 using System.Web.Mvc;
 
@@ -33,14 +34,47 @@ namespace Coffee_Shop.Controllers
             return View();
         }
 
+        public ActionResult PasswordMismatch()
+        {
+            ViewBag.message = "Oops, Something went wrong, check yourself before you wreck yourself";
+            return View();
+        }
+
         public ActionResult Summary(User u)
         {
-            ViewBag.First = u.Firstname;
-            ViewBag.Last = u.Lastname;
-            ViewBag.Email = u.Email;
-            ViewBag.Pass = u.Password;
-            ViewBag.Phone = u.Phone;
-            return View();
+            var num = new HashSet<int> { 1,2,3,4,5,6,7,8,9,0};
+            string nums = "0123456789";
+
+            if (u.Password == null || u.Password != u.ConfirmPassword)
+            {
+                ViewBag.message = "Password does not match. Re-enter Password";
+                return RedirectToAction("PasswordMismatch");
+            }
+            else if (u.Email == null)
+            {
+                ViewBag.message = "Invalid email address. Please enter a valid email";
+                return RedirectToAction("Registration");
+            }
+            else if (u.Firstname == null)
+            {
+                ViewBag.message = "First name field cannot be left empty";
+                return RedirectToAction("PasswordMismatch");
+            }
+            else if (u.Lastname == null)
+            {
+                ViewBag.message = "Last name field cannot be left empty";
+                return RedirectToAction("PasswordMismatch");
+            }
+            else
+            {
+                ViewBag.First = u.Firstname;
+                ViewBag.Last = u.Lastname;
+                ViewBag.Email = u.Email;
+                ViewBag.Pass = u.Password;
+                ViewBag.ConfirmPassword = u.ConfirmPassword;
+                ViewBag.Phone = u.Phone;
+                return View();
+            }
         }
     }
 }
--- Coffee Shop/Models/User.cs
+++ Coffee Shop/Models/User.cs
@@ -2,15 +2,34 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.ComponentModel.DataAnnotations;
 
 namespace Coffee_Shop.Models
 {
     public class User
     {
+        [Required]
+        [StringLength(36, MinimumLength = 2)]        
         public string Firstname { get; set; }
+
+        [Required]
+        [StringLength(36, MinimumLength = 2)]
         public string Lastname { get; set; }
+
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
+
+        [Required]
+        [Phone]
         public string Phone { get; set; }
+
+        [Required]
+        [StringLength(36, MinimumLength = 8)]
         public string Password { get; set; }
+
+        [Required]
+        [StringLength(36, MinimumLength = 8)]
+        public string ConfirmPassword { get; set; }
     }
 }
